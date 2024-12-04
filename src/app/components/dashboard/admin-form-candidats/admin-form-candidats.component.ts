cfbcb1c185c31f4e7c4654028ba7bb1f0c827e2f
tbtaleb <tbfeeber@gmail.com>
2024-12-04 17:16:11 +0100

    ehh dashboard w form ta3 candidate

--- src/app/components/dashboard/admin-form-candidats/admin-form-candidats.component.ts
+++ src/app/components/dashboard/admin-form-candidats/admin-form-candidats.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-form-candidats',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './admin-form-candidats.component.html',
   styleUrl: './admin-form-candidats.component.css'
 })
 export class AdminFormCandidatsComponent {
+  candidateForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.candidateForm = this.fb.group({
+      name: ['', Validators.required],
+      party: ['', Validators.required],
+      biography: ['', Validators.required],
+      electoralProgram: ['', Validators.required],
+      profilePicture: [''],
+    });
+  }
+
+  onSubmit(){
+    if (this.candidateForm.valid) {
+      console.log(this.candidateForm.value);
+      alert('Form submitted successfully!');
+    } else {
+      alert('Please fill out all required fields.');
+    }
+  }
 }
--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AdminListCandidatsComponent } from "../admin-list-candidats/admin-list-candidats.component";
+import { AdminFormCandidatsComponent } from "../admin-form-candidats/admin-form-candidats.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [AdminListCandidatsComponent],
+  imports: [AdminListCandidatsComponent, AdminFormCandidatsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
